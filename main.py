bb1007ef3c0e470b960baeed3783264655bbeb7a
Shane <sdolleris33@gmail.com>
2020-11-17 17:33:13 +0000

    final rev

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 from datetime import datetime
 import random
-import sys
 
 
 #global var
-
-NAV_CONT = ('games', 'cart', 'buy', 'admin', 'exit')
+cart_items = []
+cart_prices = []
+NAV_CONT = ('games', 'add', 'buy', 'admin', 'exit')
    
-game_inv = [                     #product inventory compiled list title,stock,price
+#product inventory compiled list: title,stock,price
+game_inv = [                     
 	[1,'Ghost of Tsushima',60],
 	[2,'Grand Theft Auto 5',35],
 	[3,'Spider Man      ',20],
 	[4,'Call of Duty   ',20],
 	[5,'Mortal Kombat 11',20]
-	
 ]
 
-cart_items = []
-cart_sum = []
-grand_total = sum(cart_sum)
+
+def display_games():
+	"""Print formatted game inventory"""
+	
+	print("# \tTitle\t\tPrice")
+	for game in game_inv:
+		print(f"{game[0]} {game[1]}\t{game[2]}")
 
 
 def menu():
@@ -27,25 +31,40 @@ def menu():
 	print("""\t  ---GameRx--- 
 	"Dose up on Games"\n
 	-View our Games (Type games)
-	-Add to Cart (Type cart)  
+	-Add to Cart (Type add)  
 	-Checkout (Type buy)   
 	-Admin(Type admin)   
 	-Exit (Type exit)\n""")
 	
+def game_names(list):
+	"""Interates of list returning index value"""
+	return[x[1] for x in list]
+			
 	
 	
-def cart_logs(item,num):
-	"""adds game to cart_items and game cost to cart_sum"""
+def cart_logs(item):
+	"""adds game to cart_items and game price to cart_sum"""
 	
-	cart_items.append(item[num])
-	cart_sum.append(item[2])
-	return len(cart_items)
+	item = int(item) - 1
+	price = game_inv[item][2]
+	cart_items.append(game_inv[item])
+	cart_prices.append(price)
+	
+def order_log(list,str):
+	"""formated string in buy() with datetime"""
+	
+	print(f"""---GameRx---
+			--Order Summary--	
+		Date: {datetime.now()}
+		Customer:{str}
+		Games:{game_names(list)}
+	Total: ${float(sum(cart_prices))}
+	""")
+	
+	
+	
 		
-def display_games():
-	"""Print formatted game inventory"""
-	print("# \tTitle\t\tPrice")
-	for game in game_inv:
-		print(f"{game[0]} {game[1]}\t{game[2]}")
+
 
 
 
@@ -59,26 +78,32 @@ while True:
 		print(f'OOPS! You can only type {NAV_CONT}')
 	elif nav == 'games':
 		display_games()
-	elif nav == 'cart':
+	elif nav == 'add':
 		while True:
 			display_games()
 			select = int(input('What would you like to add to you cart; Type in the #? '))
 			for i,t,p in game_inv:
 				if select == i:
-					i = i - 1
-					cart_logs(game_inv,i)
-					print(f'{t} ${p}')
+					cart_logs(select)
+					print(f'Game added: {t} ${p}')
 					print(f'Cart items: {len(cart_items)}')
-					print(cart_items)
 			cart_cfm = input('Finished adding to cart? (y/n): ').lower()
 			if cart_cfm == 'y':
 				break
 	elif nav == 'buy':
 		if len(cart_items) == 0:
-			print('You have no items in you cart')
+			print('You have no items in your cart')
 		else:
-			print(f'Cart:\n {cart_items}')
-			
+			print(f'# of items in Cart: {len(cart_items)} Total: ${sum(cart_prices)}')
+			buy_cfm = input('Would you like to finalize your purchase? y/n ').lower()
+			if buy_cfm == 'y':
+				print('---Writing Bill---')
+				name = input('What is your name? ')
+				order_log(cart_items,name)
+				print(f'Thank you for your business {name}')
+				break
+			else:
+				print('Your order is ready when you are!')
 	elif nav == 'admin':
 		username = input("Enter Admin UserID: ")
 		password = input("Enter Password: ")
@@ -100,8 +125,4 @@ while True:
 		
 
 		   
-		   		
-	
-
-
-
+		  
