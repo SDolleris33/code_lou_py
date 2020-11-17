db64339bbb1d8e567ebb4c6285c311fc79c4dd07
Shane <sdolleris33@gmail.com>
2020-11-17 06:14:43 +0000

    main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+
+#product inventory compiled list x,x,x,x
+prod_inv = [[],[],[]] 
+
+#Menu Layout defines input values
+def menu():
+	print('\t    ***GameRx***\n\t  "Dose up on Games"\n\n\tView our Games (Type show)\n\t  Add to Cart (Type add)\n\t   Checkout (Type buy)\n\t    Exit (Type exit)')
+
+#Cart
+
+
+#Invoice
+
+
+
+
+#Master Loop
+
+while True:
+	menu()
+#navigational input
+	nav = str(input().lower)
+	
+
+
+
